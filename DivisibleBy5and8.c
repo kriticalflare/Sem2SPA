cf8ce22c312c17826d93e301953ff806956facf9
Mithil Poojary <mithil467@gmail.com>
2019-02-02 17:30:40 +0530

    Update DivisibleBy5and8.c

--- DivisibleBy5and8.c
+++ DivisibleBy5and8.c
@@ -1,18 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main()
 {
     int num=1,i=1;
     while(i<=10)
     {
-        if((num%5==0)&&(num%8==0))
+        if( num%5 == 0 && num%8 == 0)
         {
-            printf("%d is a divisble by both 5 and 8\n",num);
+            printf("%d is divisble by both 5 and 8\n",num);
             num=num+1;
-            i=i+1;
-
+            i++;
         }
         else
-            num=num+1;
+            num++;
     }
     return 0;
 }
