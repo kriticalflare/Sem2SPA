bf968140c61bae4b6326f251d5fd6c440f539e3e
sasukeuzumaki31 <43280927+sasukeuzumaki31@users.noreply.github.com>
2019-02-04 20:55:25 +0530

    Add files via upload
    another pattern....
    ps:-how many more?? :'-(

---
+++ pattern3.c
@@ -0,0 +1,32 @@
+/*program to print pattern :-
+                      *
+                     ***
+                    *****
+                   *******
+                   .
+                   .
+                   .
+                   upto n
+
+                    */
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter n\n ");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n-i;j++)
+        {
+            printf(" ");
+
+        }
+        for(int k=1;k<=2*i-1;k++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
