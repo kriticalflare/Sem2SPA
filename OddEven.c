70d270bd2762b47aa8c51b052031a3a3ec91cb46
s01idsnake <krithiksuvarna@gmail.com>
2019-04-21 14:11:10 +0530

    SPA practical programs

--- OddEven.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-  int num;
-  printf("Enter number : ");
-  scanf("%d",&num);
-  num%2?printf("Odd"):printf("Even");
-}
--- fibonacci.c
+++ fibonacci.c
@@ -1,22 +1,16 @@
-/*Fibonacci Sequence
-
-Whats Fibonacci Sequence ?
-https://www.mathsisfun.com/numbers/fibonacci-sequence.html
-
-*/
-
 #include<stdio.h>
+
 int main()
 {
-    int nextterm,term1=1,term2=1,i,n;
-    printf("How many terms of the sequence do you want?\n\n");
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
-    {
-        printf("%d",term1);
-        nextterm=term1+term2;
-        term1=term2;
-        term2=nextterm;
-    }
-    return 0;
-}
+	int n , i , t1 = 1 , t2 = 1, nt ;
+	printf("Enter the length of the series\n");
+	scanf("%d",&n);
+	for ( i = 0 ; i < n ; i++)
+	{
+		printf("%d  ", t1 );
+		nt = t1 + t2;
+		t1 = t2;
+		t2 = nt;
+	}
+	return 0;
+} 
